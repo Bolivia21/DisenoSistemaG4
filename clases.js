8dce86912569a30812b9fe19e5cc6da918ee5c93
Nicolas <90664@sistemas.frc.utn.edu.ar>
2023-05-11 21:16:16 -0300

    agregar proyecto

--- clases.js
+++ clases.js
@@ -1,1 +1,39 @@
-console.log("Hola mundo")
+class Llamada {
+    constructor(descripcionOperador,detalleAccionRequerida,
+        duracion,encuestaEnviada,observacionAuditor){
+            this.descripcionOperador = descripcionOperador;
+            this.detalleAccionRequerida = detalleAccionRequerida;
+            this.duracion = duracion;
+            this.encuestaEnviada = encuestaEnviada;
+            this.observacionAuditor = observacionAuditor
+        }
+    getFechaEstadoActual(fecha){ 
+        fechaEstado = fecha
+        return fechaEstado
+    }
+    actualizarEnCurso(){
+
+    }
+    getValidaciones(){
+
+    }
+    getClientesPorDni(){
+
+    }
+    getDatosLlamadas(){
+
+    }
+    esCorrecta(){
+
+    }
+    setDescripcionOperador(){
+
+    }
+    actualizarFinalizado(){
+
+    }
+    setDuracion(){
+
+    }
+}
+
