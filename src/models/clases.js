6616f50fa06eab868c8911e44cfa65dd8d803302
Federico Gimenez <fedegimenez144@gmail.com>
2023-05-13 19:26:24 -0300

    Cliente terminado

--- src/models/clases.js
+++ src/models/clases.js
@@ -91,7 +91,7 @@ class OpcionValidacion{
     }
 }
 
-class Cliente{ //cliente terminado
+class Cliente{ //cliente terminadoa
     constructor(dni, nombreCompleto, nroCelular, info){
         this.dni = dni;
         this.nombreCompleto = nombreCompleto;
