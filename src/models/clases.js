05dbbae7ae4d5d9980a8ec49f48e4e618c0ba942
benjamin pavon <pavonbenja@gmail.com>
2023-05-13 20:17:37 -0300

    metodos estados funcionando

--- src/models/clases.js
+++ src/models/clases.js
@@ -42,13 +42,27 @@ class Estado{
     constructor(nombre){
         this.nombre = nombre;
     }
-    esEnCurso(){
-
+    esEnCurso(estados){
+        for(let i = 0; i < estados.length ; i++){
+            if(estados[i]=== "enCurso"){
+                return true
+            }
+        return false 
+        }
     }
-    esFinalizado(){
-
+    esFinalizado(estados){
+        for(let i = 0; i < estados.length ; i++){
+            if(estados[i]=== "esFinalizada"){
+                return true
+            }
+        return false 
+        }
     }
 }
+
+const estados = new Array("enCurso", "esCancelada", "esFinalizada", "esInicializada");
+const verificarEstado = new Estado("enCurso");
+console.log(verificarEstado.esEnCurso(estados))
 
 class subOpcionLlamada{
     constructor(nombre, nroOrden,validacionRequerida){
